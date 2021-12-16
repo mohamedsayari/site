e180117b0335966cdf4078ba806fb28db1b76f2c
mohamed sayari <medsayri2@gmail.com>
2021-12-16 18:43:44 +0100

    12

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
+
+import {NgForm} from '@angular/forms';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,17 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm = new FormGroup({
+   
+  });
+ submitted = false;
 
-  constructor() { }
+    constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
-signup( form : any) {
-  console.log( form)
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+    }
+    
+    getData(x:any){
+        console.log(x);}
 }
-onChangeEvent(event: any){
 
-  console.log(event.target.value);
-
-}
-}
